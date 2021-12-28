957c2f5648b837a852dc38f3437d870553615497
MichaelDelcambre <delcambrem@gmail.com>
2021-12-28 16:00:11 -0600

    commit tree e367e2a8a92c633adaf3616ba93df4496b2ffbfe

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
-function MainContainer() {
+function MainContainer({addStockCard}) {
+
+const [stocks, setStocks] = useState([]);
+
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+    .then((r) => r.json())
+    .then((stocks) => setStocks(stocks))
+  }, [])
+
+  
+  function addStockCard (e) {
+    console.log(e.target.value)
+  }
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks} />
         </div>
         <div className="col-4">
           <PortfolioContainer />
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-function Stock() {
+function Stock({stock}) {
+// console.log({stock})
+  function addStockCard (e) {
+    console.log(e.target.value)
+  }
   return (
     <div>
-      <div className="card">
+      <div className="card" onClick={(e) => addStockCard(stock.id)}>
         <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+          <h5 className="card-title">{stock.name}</h5>
+          <p className="card-text">{stock.price}</p>
         </div>
       </div>
     </div>
